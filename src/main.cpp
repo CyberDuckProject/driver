bf9333f3fedcfbbeb6e9fd9d6e90bd8e9b68b2ab
Adam Soltan <adam.henryk.soltan@gmail.com>
2022-01-21 17:24:49 +0100

    Add streaming code from BME280 example

--- src/main.cpp
+++ src/main.cpp
@@ -46,6 +46,49 @@ i8 spi_write(u8 reg_addr, const u8* reg_data, u32 len, void* intf_ptr)
     return 0;
 }
 
+void print_sensor_data(struct bme280_data* comp_data)
+{
+#ifdef BME280_FLOAT_ENABLE
+    printf("%0.2f, %0.2f, %0.2f\r\n", comp_data->temperature, comp_data->pressure,
+           comp_data->humidity);
+#else
+    printf("%ld, %ld, %ld\r\n", comp_data->temperature, comp_data->pressure, comp_data->humidity);
+#endif
+}
+
+int8_t stream_sensor_data_normal_mode(struct bme280_dev* dev)
+{
+    int8_t rslt;
+    uint8_t settings_sel;
+    struct bme280_data comp_data;
+
+    /* Recommended mode of operation: Indoor navigation */
+    dev->settings.osr_h = BME280_OVERSAMPLING_1X;
+    dev->settings.osr_p = BME280_OVERSAMPLING_16X;
+    dev->settings.osr_t = BME280_OVERSAMPLING_2X;
+    dev->settings.filter = BME280_FILTER_COEFF_16;
+    dev->settings.standby_time = BME280_STANDBY_TIME_62_5_MS;
+
+    settings_sel = BME280_OSR_PRESS_SEL;
+    settings_sel |= BME280_OSR_TEMP_SEL;
+    settings_sel |= BME280_OSR_HUM_SEL;
+    settings_sel |= BME280_STANDBY_SEL;
+    settings_sel |= BME280_FILTER_SEL;
+    rslt = bme280_set_sensor_settings(settings_sel, dev);
+    rslt = bme280_set_sensor_mode(BME280_NORMAL_MODE, dev);
+
+    printf("Temperature, Pressure, Humidity\r\n");
+    while (1)
+    {
+        /* Delay while the sensor completes a measurement */
+        dev->delay_us(70000, dev->intf_ptr);
+        rslt = bme280_get_sensor_data(BME280_ALL, &comp_data, dev);
+        print_sensor_data(&comp_data);
+    }
+
+    return rslt;
+}
+
 int main()
 {
     io::init();
@@ -67,6 +110,8 @@ int main()
     dev.delay_us = delay_us;
     result = bme280_init(&dev);
     BOOST_LOG_TRIVIAL(debug) << "result: " << result;
+
+    stream_sensor_data_normal_mode(&dev);
 
     // shutdown spi
     spiClose(bme280);
