3b96884987857184653bf4c393be15977066f139
Adam Soltan <adam.henryk.soltan@gmail.com>
2022-01-29 08:50:48 +0100

    Change BME280 bus

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 
 int main()
 {
-    motor left{13};
-    motor right{12};
-    output_pin eyes{6};
-    output_pin fan{7};
-    adc ain{false, 0, 3.3f};
-    // bme280 env{2, 0x76, {21, 19}};
+    // motor left{13};
+    // motor right{12};
+    // output_pin eyes{6};
+    // output_pin fan{7};
+    // adc ain{false, 0, 3.3f};
+    bme280 env{22, 0x76, {19, 21}};
 }
