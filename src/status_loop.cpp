a5d4c3c7bb629b77cdcc7dc8c37e055239ed572d
Adam Soltan <adam.henryk.soltan@gmail.com>
2022-02-23 09:00:18 +0100

    Split status_loop into .h and .cpp

---
+++ src/status_loop.cpp
@@ -0,0 +1,44 @@
+#include "status_loop.h"
+
+#include <boost/bind/bind.hpp>
+#include <boost/log/trivial.hpp>
+
+namespace asio = boost::asio;
+using udp = asio::ip::udp;
+
+status_loop::status_loop(asio::thread_pool& ctx, ::io& io, std::chrono::milliseconds period) :
+    io{io}, timer{ctx, period}, period{period}, socket{ctx, udp::v4()}
+{
+    timer.async_wait(boost::bind(&status_loop::wait_handler, this));
+}
+
+void status_loop::connect(const udp::endpoint& endpoint)
+{
+    std::lock_guard lock{mutex};
+    remote = endpoint;
+}
+
+void status_loop::wait_handler()
+{
+    std::lock_guard lock{mutex};
+    if (remote.has_value())
+    {
+        if (!io.eyes())
+        {
+            io.set_eyes(true);
+        }
+        loop();
+    }
+    else
+    {
+        io.set_eyes(!io.eyes());
+    }
+
+    timer.expires_at(timer.expiry() + period);
+    timer.async_wait(boost::bind(&status_loop::wait_handler, this));
+}
+
+void status_loop::loop()
+{
+    // TODO: send data
+}
--- src/status_loop.h
+++ src/status_loop.h
@@ -3,59 +3,25 @@
 
 #include "io.h"
 #include <boost/asio.hpp>
-#include <boost/bind/bind.hpp>
+#include <optional>
 
-template<typename executor_type>
 class status_loop
 {
 public:
-    status_loop(executor_type& ctx, io& io, std::chrono::milliseconds period) :
-        timer{ctx, period}, socket{ctx}, io{io}, period{period}
-    {
-        timer.async_wait(boost::bind(&status_loop::loop, this));
-    }
+    status_loop(boost::asio::thread_pool& ctx, io& io, std::chrono::milliseconds period);
 
-    void connect(const boost::asio::ip::udp::endpoint& endpoint)
-    {
-        socket.async_connect(endpoint, [&](const boost::system::error_code& ec) {
-            if (ec)
-            {
-                throw boost::system::system_error{ec};
-            }
-            connected = true;
-        });
-    }
+    void connect(const boost::asio::ip::udp::endpoint& endpoint);
 
 private:
-    void send_status()
-    {
-        // TODO: send sensor data
-    }
+    void wait_handler();
+    void loop();
 
-    void loop()
-    {
-        if (connected)
-        {
-            if (!io.eyes())
-            {
-                io.set_eyes(true);
-            }
-            send_status();
-        }
-        else
-        {
-            io.set_eyes(!io.eyes());
-        }
-        
-        timer.expires_at(timer.expiry() + period);
-        timer.async_wait(boost::bind(&status_loop::loop, this));
-    }
-
-    boost::asio::steady_timer timer;
-    boost::asio::ip::udp::socket socket;
     ::io& io;
+    boost::asio::steady_timer timer;
     std::chrono::milliseconds period;
-    std::atomic_bool connected{false};
+    boost::asio::ip::udp::socket socket;
+    std::optional<boost::asio::ip::udp::endpoint> remote;
+    std::mutex mutex;
 };
 
 #endif
