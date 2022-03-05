017a9d0859cd178652af2f3aa8a4572116474030
Jan Bielak <contact@janbielak.com>
2022-03-05 12:01:52 +0100

    Log setter calls in mock driver

--- src/mock/io.cpp
+++ src/mock/io.cpp
@@ -15,6 +15,7 @@ void io::set_left_motor(f32 speed)
 {
     assert(speed >= 0.0f && speed <= 1.0f);
     current_left_speed = speed;
+    BOOST_LOG_TRIVIAL(info)  << "left motor speed set to " << speed;
 }
 
 f32 io::left_motor() const
@@ -26,6 +27,7 @@ void io::set_right_motor(f32 speed)
 {
     assert(speed >= 0.0f && speed <= 1.0f);
     current_right_speed = speed;
+    BOOST_LOG_TRIVIAL(info)  << "right motor speed set to " << speed;
 }
 
 f32 io::right_motor() const
@@ -36,6 +38,7 @@ f32 io::right_motor() const
 void io::set_eyes(bool on)
 {
     current_eyes_state = on;
+    BOOST_LOG_TRIVIAL(info)  << "eyes state set to " << on;
 }
 
 bool io::eyes() const
@@ -46,6 +49,7 @@ bool io::eyes() const
 void io::set_fan(bool on)
 {
     current_fan_state = on;
+    BOOST_LOG_TRIVIAL(info)  << "fan state set to " << on;
 }
 
 bool io::fan() const
@@ -56,6 +60,7 @@ bool io::fan() const
 void io::set_heartbeat(bool on)
 {
     current_heartbeat_state = on;
+    BOOST_LOG_TRIVIAL(info)  << "heartbeat state set to " << on;
 }
 
 bool io::heartbeat() const
