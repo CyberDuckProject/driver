1e67be0d9170945d4291836524fa57d66c195c21
Adam Soltan <adam.henryk.soltan@gmail.com>
2022-04-16 20:29:51 +0200

    Implement BME280

--- src/dev/bme280.cpp
+++ src/dev/bme280.cpp
@@ -1,6 +1,7 @@
 #include "bme280.h"
 
 #include <bme280.h>
+#include <cassert>
 #include <pigpio.h>
 #include <unistd.h>
 
@@ -9,10 +10,16 @@ namespace {
 
 i8 i2c_read(u8 reg_addr, u8* reg_data, u32 len, void* intf_ptr)
 {
+    assert(intf_ptr);
+    i32 handle = *static_cast<i32*>(intf_ptr);
+    return i2cReadI2CBlockData(handle, reg_addr, (char*)reg_data, len) == len ? 0 : -1;
 }
 
 i8 i2c_write(u8 reg_addr, const u8* reg_data, u32 len, void* intf_ptr)
 {
+    assert(intf_ptr);
+    i32 handle = *static_cast<i32*>(intf_ptr);
+    return i2cWriteI2CBlockData(handle, reg_addr, (char*)reg_data, len) == 0 ? 0 : -1;
 }
 
 void i2c_delay_us(u32 period, void*)
@@ -24,9 +31,12 @@ void i2c_delay_us(u32 period, void*)
 
 bme280::bme280(u32 bus, u32 address)
 {
+    // Open I2C bus
+    handle = i2cOpen(bus, address, 0);
+
     // Setup BME280 interface
     device.intf = BME280_I2C_INTF;
-    device.intf_ptr = ? ;
+    device.intf_ptr = &handle;
     device.read = i2c_read;
     device.write = i2c_write;
     device.delay_us = i2c_delay_us;
@@ -48,6 +58,10 @@ bme280::bme280(u32 bus, u32 address)
 
 bme280::~bme280()
 {
+    if (handle >= 0)
+    {
+        i2cClose(handle);
+    }
 }
 
 bme280_data bme280::get_sensor_data() const
