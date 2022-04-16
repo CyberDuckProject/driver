de611a65ba416f67b355f6efa5c7b72ae943beb6
Adam Soltan <adam.henryk.soltan@gmail.com>
2022-04-16 20:39:17 +0200

    Implement binary_pin

---
+++ src/dev/binary_pin.cpp
@@ -0,0 +1,29 @@
+#include "binary_pin.h"
+
+#include <pigpio.h>
+
+namespace dev {
+
+binary_pin::binary_pin(u32 broadcom, bool on) : broadcom{broadcom}
+{
+    gpioSetMode(broadcom, PI_OUTPUT);
+    set_state(on);
+}
+
+binary_pin::~binary_pin()
+{
+    gpioWrite(broadcom, false);
+}
+
+bool binary_pin::state() const
+{
+    return value;
+}
+
+void binary_pin::set_state(bool on)
+{
+    value = on;
+    gpioWrite(broadcom, on);
+}
+
+} // namespace dev
---
+++ src/dev/binary_pin.h
@@ -0,0 +1,24 @@
+#ifndef DEV_BINARY_PIN_H
+#define DEV_BINARY_PIN_H
+
+#include "fundamental_types.h"
+
+namespace dev {
+
+class binary_pin
+{
+public:
+    explicit binary_pin(u32 broadcom, bool on = false);
+    ~binary_pin();
+
+    bool state() const;
+    void set_state(bool on);
+
+private:
+    u32 broadcom;
+    bool value;
+};
+
+} // namespace dev
+
+#endif
