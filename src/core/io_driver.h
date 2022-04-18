e832e275654d476ce5da686760f6357176788cc9
Adam Soltan <adam.henryk.soltan@gmail.com>
2022-04-18 09:21:27 +0200

    Implement sensor readouts

--- src/core/io_driver.h
+++ src/core/io_driver.h
@@ -9,6 +9,13 @@
 class io_driver
 {
 public:
+    f64 temperature() const;
+    f64 turbidity() const;
+    f64 dust(u32 iterations = 10) const;
+    f64 battery_voltage() const;
+
+    bme280_data weather() const;
+
 private:
     esc left{13};
     esc right{12};
@@ -16,7 +23,7 @@ private:
     binary_pin fan{7, true};
     binary_pin eyes{6, true};
     mcp3008 adc{false, 0, 3.3};
-    mutable binary_pin dust{5};
+    mutable binary_pin dust_ir{5};
     bme280 humidity{22, 0x76};
 };
 
