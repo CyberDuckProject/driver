ad00217d7691b2ad44e439659fca0eafa8927c4b
Adam Soltan <adam.henryk.soltan@gmail.com>
2022-04-18 13:00:05 +0200

    Make aliases more flexible

--- src/utl/asio.h
+++ src/utl/asio.h
@@ -4,7 +4,7 @@
 #include <boost/asio.hpp>
 
 using execution_context = boost::asio::thread_pool;
-using executor = boost::asio::thread_pool::executor_type;
-using strand = boost::asio::strand<boost::asio::thread_pool::executor_type>;
+using executor = execution_context::executor_type;
+using strand = boost::asio::strand<executor>;
 
 #endif
