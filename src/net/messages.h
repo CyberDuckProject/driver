288f89d6355709cf73157f3d418feb0fd5bfcde1
Adam Soltan <adam.henryk.soltan@gmail.com>
2022-04-20 14:18:14 +0200

    Add TCP messages

---
+++ src/net/messages.h
@@ -0,0 +1,23 @@
+#ifndef NET_MESSAGES_H
+#define NET_MESSAGES_H
+
+#include "utl/fundamental_types.h"
+
+struct control_message
+{
+    f64 left;
+    f64 right;
+};
+
+struct status_message
+{
+    f64 water_temperature;
+    f64 water_turbidity;
+    f64 dust;
+    f64 battery_voltage;
+    f64 pressure;
+    f64 temperature;
+    f64 humidity;
+};
+
+#endif
