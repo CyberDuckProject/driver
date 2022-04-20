624df56f4b3bb424de4a7ca673bc2f0be9314c43
Adam Soltan <adam.henryk.soltan@gmail.com>
2022-04-20 19:27:56 +0200

    Start implementing message_buffer

--- src/net/messages.h
+++ src/net/messages.h
@@ -2,6 +2,14 @@
 #define NET_MESSAGES_H
 
 #include "utl/fundamental_types.h"
+#include <boost/asio.hpp>
+#include <variant>
+
+enum class message_type : u8
+{
+    control = 1,
+    status = 2
+};
 
 struct control_message
 {
@@ -18,6 +26,45 @@ struct status_message
     f64 pressure;
     f64 temperature;
     f64 humidity;
+};
+
+struct message_buffer
+{
+public:
+    message_type type() const;
+
+    template<typename AsyncReadStream, typename ReadHandler>
+    void async_read(AsyncReadStream& stream, ReadHandler&& handler)
+    {
+        namespace asio = boost::asio;
+        namespace bs = boost::system;
+
+        auto async_read = [&stream](asio::mutable_buffer buffer, auto&& handler) {
+            asio::async_read(stream, buffer, std::forward<decltype(handler)>(handler));
+        };
+
+        // Read the messag header
+        async_read(header(), [this, &handler](const bs::error_code& ec, std::size_t n) {
+            if (!ec)
+            {
+                // Read the message body
+                async_read(body(), [this, &handler](const bs::error_code& ec, std::size_t n) {
+                    handler(ec, n + sizeof(header));
+                })
+            }
+            else
+            {
+                handler(ec, n);
+            }
+        });
+    }
+
+private:
+    boost::asio::mutable_buffer header();
+    boost::asio::mutable_buffer body();
+
+    message_type type;
+    std::variant<control_message, status_message> value;
 };
 
 #endif
