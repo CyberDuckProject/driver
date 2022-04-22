b9100ebbec08e75fff2a1c67d4cf9236b523c737
Adam Soltan <adam.henryk.soltan@gmail.com>
2022-04-22 11:25:56 +0200

    Implement message buffer

--- src/net/message_buffer.h
+++ src/net/message_buffer.h
@@ -11,6 +11,24 @@ namespace detail {
 template<typename T, typename... Ts>
 constexpr bool contains = (std::is_same_v<T, Ts> || ...);
 
+struct type_visitor
+{
+    message_type operator()(const std::monostate&)
+    {
+        return {};
+    }
+
+    message_type operator()(const control_message&)
+    {
+        return message_type::control;
+    }
+
+    message_type operator()(const status_message&)
+    {
+        return message_type::status;
+    }
+};
+
 } // namespace detail
 
 template<typename... Messages>
@@ -20,39 +38,58 @@ public:
     message_buffer() = default;
 
     template<typename Message>
-    explicit message_buffer(Message msg)
+    explicit message_buffer(const Message& msg)
     {
+        *this = msg;
     }
 
     template<typename Message>
-    message_buffer& operator=(Message msg)
+    message_buffer& operator=(const Message& msg)
     {
+        static_assert(detail::contains<Message, Messages...>);
+        value = msg;
+        return *this;
     }
 
     bool empty() const
     {
+        return std::holds_alternative<std::monostate>(value);
     }
 
     void clear()
     {
+        value = std::monostate{};
     }
 
-    const message_type& type() const
+    message_type type() const
     {
+        assert(!empty());
+        return std::visit(detail::type_visitor{}, value);
     }
 
     template<typename Message>
-    auto& get()
+    Message& get()
     {
+        assert_get<Message>();
+        return std::get<Message>(value);
     }
 
     template<typename Message>
-    const auto& get() const
+    const Message& get() const
     {
+        assert_get<Message>();
+        return std::get<Message>(value);
     }
 
 private:
-    message_type msg_type;
+    template<typename Message>
+    void assert_get() const
+    {
+        static_assert(detail::contains<Message, Messages...>);
+        assert(!empty());
+        assert(std::holds_alternative<Message>(value));
+    }
+
     std::variant<std::monostate, Messages...> value{std::monostate{}};
 };
 
