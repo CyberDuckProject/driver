0cf975c8944fa02aefc2e8d4100a0e4b56524700
Adam Soltan <adam.henryk.soltan@gmail.com>
2022-04-29 13:38:46 +0200

    Add temporary controls

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,39 @@
 
 namespace asio = boost::asio;
 
+msg::message_buffer<msg::control_message> buffer;
+
+template<typename ExecutionContext>
+void read(net::connection<ExecutionContext>& conn);
+
+template<typename ExecutionContext>
+void accept(net::connection<ExecutionContext>& conn)
+{
+    conn.async_accept([&](...) {
+        BOOST_LOG_TRIVIAL(debug) << "connected";
+        read(conn);
+    });
+}
+
+template<typename ExecutionContext>
+void read(net::connection<ExecutionContext>& conn)
+{
+    conn.async_read_message(buffer, [&](boost::system::error_code ec, std::size_t) {
+        if (!ec)
+        {
+            auto& m = buffer.get<msg::control_message>();
+            BOOST_LOG_TRIVIAL(debug) << m.left << " " << m.right;
+            read(conn);
+        }
+        else
+        {
+            BOOST_LOG_TRIVIAL(debug) << ec.message();
+            BOOST_LOG_TRIVIAL(debug) << "disconnected";
+            accept(conn);
+        }
+    });
+}
+
 void guarded_main()
 {
     BOOST_LOG_TRIVIAL(info) << "starting 4 threads";
@@ -13,6 +46,8 @@ void guarded_main()
 
     BOOST_LOG_TRIVIAL(info) << "listening on port 1333";
     net::connection connection{ctx, 1333};
+
+    accept(connection);
 
     ctx.join();
 }
